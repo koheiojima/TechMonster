ae894254f5eff42af89a5289a70bc243172348aa
小島光平 <sesuna1997_winter@yahoo.co.jp>
2017-04-23 18:51:03 +0900

    修正

--- TechMonster/BattleViewController.swift
+++ TechMonster/BattleViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class BattleViewController: UIViewController {
     
+    var enemyAttackTimer: Timer!
     var enemy: Enemy!
     var player: Player!
     
@@ -20,7 +21,7 @@ class BattleViewController: UIViewController {
     @IBOutlet var enemyImageView: UIImageView!
     @IBOutlet var playerImageView: UIImageView!
     
-    @IBOutlet var enemyImageView: UIImageView!
+    @IBOutlet var enemyHPBar: UIProgressView!
     @IBOutlet var playerHPBar: UIProgressView!
     
     @IBOutlet var enemyNameLabel: UILabel!
@@ -29,8 +30,78 @@ class BattleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        enemyHPBar.transform = CGAffineTransform(scaleX: 1.0, y:4.0)
+        playerHPBar.transform = CGAffineTransform(scaleX: 1.0, y:4.0)
+    
+        playerNameLabel.text = player.name
+        playerImageView.image = player.image
+        playerHPBar.progress = player.currentHP / player.maxHP
+        
+        startBattle()
     }
+    
+    func startBattle() {
+        TechDraUtil.playBGM(fileName: "BGM_battle001")
+        
+        enemy = Enemy()
+        
+        enemyNameLabel.text = enemy.name
+        enemyImageView.image = enemy.image
+        enemyHPBar.progress = enemy.currentHP / enemy.maxHP
+        
+        attackButton.isHidden = false
+        
+        enemyAttackTimer = Timer.scheduledTimer(timeInterval: enemy.attackInterval, target: self, selector: #selector(self.enemyAttack), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func playerAttack() {
+        TechDraUtil.animateDamage(enemyImageView)
+        TechDraUtil.playSE(fileName: "SE_attack")
+        
+        enemy.currentHP = enemy.currentHP - player.attackPower
+        enemyHPBar.setProgress(enemy.currentHP / enemy.maxHP, animated: true)
+    
+        if enemy.currentHP < 0 {
+            TechDraUtil.animateVanish(enemyImageView)
+            finishBattle(winPlayer: true)
+        }
+    }
+    
+    func enemyAttack(){
+        TechDraUtil.animateDamage(playerImageView)
+        TechDraUtil.playSE(fileName: "SE_attack")
+        
+        player.currentHP = player.currentHP - enemy.attackPower
+        playerHPBar.setProgress(player.currentHP / player.maxHP, animated: true)
+        
+        if player.currentHP < 0 {
+            TechDraUtil.animateVanish(playerImageView)
+            finishBattle(winPlayer: false)
+        }
+    }
+    func finishBattle(winPlayer: Bool) {
+        enemyAttackTimer.invalidate()
+        TechDraUtil.stopBGM()
+        
+        attackButton.isHidden = true
+        
+        
+        
+        let finishedMessage: String
+        if winPlayer == true {
+            TechDraUtil.playSE(fileName: "SE_fanface")
+            finishedMessage = "プレイヤーの勝利！！"
+        }else{
+            TechDraUtil.playSE(fileName: "SE_gameover")
+            finishedMessage = "プレイヤーの敗北..."
+        }
+        let alert = UIAlertController(title: "バトル終了！",message: finishedMessage, preferredStyle:UIAlertControllerStyle.alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: { action in self.dismiss(animated:  true, completion: nil)
+        })
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
